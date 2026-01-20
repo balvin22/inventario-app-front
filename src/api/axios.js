bb23c6680bbc37691d09f51e651deeeb6f110568
balvin22 <sb1181358@gmail.com>
2026-01-19 19:15:53 -0500

    Corregir axios para usar variable de entorno

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+// Lógica inteligente:
+// 1. ¿Existe una variable de entorno llamada VITE_API_URL? (En Vercel SÍ existirá).
+// 2. Si no existe, asume que estás desarrollando en local y usa el 127.0.0.1.
+const baseURL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000', // Tu backend FastAPI
+    baseURL: baseURL,
 });
 
 export default api;
